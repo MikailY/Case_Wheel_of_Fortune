23bc96bbe745056ab30b7deab12184aa898f693e
MikailY <mikaily.karadeniz@gmail.com>
2026-01-20 15:27:05 +0300

    add scale effect for golden&silver counter

--- Case_Wheel_of_Fortune/Assets/Scripts/NextZoneCounterComponent.cs
+++ Case_Wheel_of_Fortune/Assets/Scripts/NextZoneCounterComponent.cs
@@ -5,16 +5,26 @@ public class NextZoneCounterComponent : MonoBehaviour
 {
     [SerializeField] private TMP_Text text;
     [SerializeField] private CanvasGroup canvasGroup;
+    [SerializeField] private ScaleEffectComponent scaleEffectComponent;
+
+    private int _index;
 
     public void Set(int index)
     {
+        if (_index == index) return;
+
         if (index == -1)
         {
             canvasGroup.alpha = 0;
+            _index = index;
             return;
         }
 
         canvasGroup.alpha = 1;
         text.text = index.ToString();
+
+        scaleEffectComponent?.Show();
+
+        _index = index;
     }
 }
---
+++ Case_Wheel_of_Fortune/Assets/Scripts/ScaleEffectComponent.cs
@@ -0,0 +1,30 @@
+using DG.Tweening;
+using UnityEngine;
+
+public class ScaleEffectComponent : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+    [SerializeField] private float duration = 0.3f;
+    [SerializeField] private float scaleAmount = 1.1f;
+
+    private Transform _target;
+    private Vector3 _defaultScale;
+    private Tweener _tween;
+
+    private void Awake()
+    {
+        _target = target ? target : transform;
+
+        _defaultScale = _target.localScale;
+    }
+
+    public void Show()
+    {
+        if (_tween.IsActive())
+            _tween.Kill();
+
+        _tween = _target.DOScale(_defaultScale * scaleAmount, duration)
+            .ChangeStartValue(_defaultScale)
+            .OnComplete(() => _target.DOScale(_defaultScale, duration));
+    }
+}
